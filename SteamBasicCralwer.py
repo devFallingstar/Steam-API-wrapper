14666f7ff947a4263e0fe5dc6a69f4c9eb99f49e
DevFallingstar <devfallingstar@gmail.com>
2019-05-22 12:30:52 +0900

    Update API version

--- SteamBasicCralwer.py
+++ SteamBasicCralwer.py
@@ -4,9 +4,11 @@ from requests.exceptions import Timeout, RequestException
 
 import xml.etree.ElementTree as ET
 
+
 class SteamBasicCralwer:
     def __init__(self, API_KEY):
         self.API_KEY = API_KEY
+        self.gameID_name_dict = dict()
 
     def __del__(self):
         pass
@@ -14,44 +16,47 @@ class SteamBasicCralwer:
     def getUserSteamID(self, nickname):
         nickname = str(nickname)
         API_NAME = "ResolveVanityURL"
-        API_VERSION = "v0001"
+        API_VERSION = "v1"
         API_URL = "http://api.steampowered.com/ISteamUser/{}/{}/" \
                   "?key={}&vanityurl={}".format(API_NAME, API_VERSION, self.API_KEY, nickname)
 
-
-        pass
-
-    def getUserGameDetail(self, steam_id):
+    def getUserGameDetail(self, steam_id, max=0):
         steam_id = str(steam_id)
         API_NAME = "GetOwnedGames"
-        API_VERSION = "v0001"
+        API_VERSION = "v1"
         API_FORMAT = "json"
-        API_URL = "http://api.steampowered.com/IPlayerService/{}/{}/" \
-                  "?key={}&steamid={}&format={}".format(API_NAME, API_VERSION, self.API_KEY, steam_id, API_FORMAT)
+        API_URL = "https://api.steampowered.com/IPlayerService/{}/{}/" \
+                  "?key={}&steamid={}&format={}&include_appinfo=1&include_played_free_games=1".format(API_NAME, API_VERSION, self.API_KEY, steam_id, API_FORMAT)
 
         response_raw = requests.get(API_URL)
-        response_json = response_raw.json()
-        response_game_detail = dict(response_json)['response']['games']
 
-        return response_game_detail
+        result = dict()
 
-    def getGameName(self, game_id):
-        game_id = str(game_id)
-        game_detail = self.getGameDetail(game_id)
-        print(game_detail.keys())
-        game_name = game_detail['name']
+        try:
+            response_json = response_raw.json()
+            response_game_detail = dict(response_json)['response']['games']
+        except Exception as e:
+            return None
 
-        return game_name
+        print("---- Colleting info for "+steam_id)
+        added_number = 0
+        total = len(response_game_detail)
+        for each_game_detail in response_game_detail:
+            game_appid = str(each_game_detail['appid'])
+            game_name = str(each_game_detail['name'])
+            game_playtime = float(each_game_detail['playtime_forever']) / 60.0
 
-    def getGameDetail(self, game_id):
-        game_id = game_id
-        API_URL = "http://store.steampowered.com/api/appdetails?appids={}".format(game_id)
+            result[game_name] = game_playtime
 
-        response_raw = requests.get(API_URL)
-        response_json = response_raw.json()
-        response_detail = response_json[game_id]['data']
+            added_number += 1
+            if added_number % 100 is 0 or added_number is total:
+                print(str(added_number + 1) + " of " + str(total))
 
-        return dict(response_detail)
+            if max is not 0:
+                if added_number > max:
+                    break
+
+        return result
 
     def getGroupMembersID(self, group_name, start_page=1, end_page=0):
         group_name = str(group_name)
