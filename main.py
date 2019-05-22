1b11856a5713cf7c46700e9a1ae2278c59067334
DevFallingstar <devfallingstar@gmail.com>
2019-05-22 12:31:12 +0900

    Update sample code

--- main.py
+++ main.py
@@ -10,13 +10,28 @@ API_KEY = config['DEFAULT']['API_KEY']
 def main():
     steam_crawler = SteamBasicCralwer(API_KEY=API_KEY)
 
-    # print(steam_crawler.getUserGameDetail('76561198045530158'))
+    with open('user_list.txt', 'r') as user_list_file, open("user_info.csv", "a+") as user_info_file:
+        user_id_list = list()
 
-    with open('user_list.txt', 'w') as file:
-        users_list = steam_crawler.getGroupMembersID('tradingcards', start_page=1)
+        for each_id in user_list_file:
+            user_id_list.append(each_id)
 
-        for each_user_id in users_list:
-            file.write(str(each_user_id)+'\n')
+        user_idx = 0
+        for each_line in user_id_list:
+            try:
+                result = steam_crawler.getUserGameDetail(each_line)
+            except KeyboardInterrupt:
+                print("Go to next user")
+
+
+            if result is not None:
+                for each_key, each_item in result.items():
+                    content = '{},{},play,{}\n'.format(each_line.strip(), each_key, each_item)
+                    user_info_file.write(content)
+                    user_info_file.flush()
+                print("{} of {}".format(user_idx, len(user_id_list)))
+                user_idx += 1
+
 
 
 if __name__ == '__main__':
